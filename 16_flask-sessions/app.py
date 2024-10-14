7dbc592812ae4135d5f40ff356041d9080b47868
Tim <timngstuy@gmail.com>
2024-10-14 19:10:33 -0400

    OMFG AFTER 3 YEARS, something is working i guess

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -5,26 +5,36 @@
 # o
 
 from flask import Flask, render_template, request, session #this one stores like verything
-import os
+import os 
 from utl import ants
 
 
 app = Flask(__name__)
 app.secret_key = ants.poop()
 
-@app.route("/",  methods=['GET', 'POST'])
+@app.route("/",  methods=['GET','POST'])
 def disp_loginpage():
+    if 'username' and 'password' in session:
+        return render_template('response.html')
     return render_template( 'login.html' ) #renders homepage
 
 
 @app.route("/auth", methods=['POST'])
 def post():
-    session['cheeks'] = request.form['username']
+    session['username'] = request.form['username']
     session['password'] = request.form['password']
-#     return render_template('response.html', inputted=inputted)
-    return "poop"
+    name = session['username']
+    return render_template('response.html', name = name)
 
+#@app.route("/login", methods =['GET', 'POST'])
+#def disp_loggedin():
+#    return "hello"
 
+@app.route("/logout", methods = ['GET', 'POST'])
+def logout():
+    session.pop('username', None)
+    session.pop('password', None)
+    return render_template('login.html')
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
