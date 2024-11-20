3986e1b28e5f275ec567c0bb485bcbbdd3d3683c
Tim <tng50@stuy.edu>
2024-11-20 11:47:17 -0500

    worked on app.py

---
+++ 23_rest/app.py
@@ -0,0 +1,10 @@
+'''
+Drizzling - Tim Ng, Endrit 
+SoftDev
+K23 - API Keys
+2024-11-20
+time spent: N/A
+'''
+# link: https://api.nasa.gov/planetary/apod?api_key=kRD0mxb0BNq9WJ7xae0BbednkdAwdg2qBR3g0iWZ
+from flask import Flask, render_template, request, session
+import os
