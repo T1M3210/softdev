ee0095811fa49fdd20e16bbd176f77cb23228e69
Tim <timngstuy@gmail.com>
2024-11-21 04:58:51 -0500

    finished with 23_rest, altered 11_flask by accident

---
+++ 23_rest/README.md
@@ -0,0 +1,7 @@
+# DISCO 
+
+# QCC
+- huge amounts of overlap regarding the functions provided 
+- don't need to use every function provided as they all cummulatively can do the same thing 
+- json.loads turns string into dictionary
+- json.dumps turns dictionary into a string 
--- 23_rest/app.py
+++ 23_rest/app.py
@@ -3,8 +3,30 @@ Drizzling - Tim Ng, Endrit
 SoftDev
 K23 - API Keys
 2024-11-20
-time spent: N/A
+time spent: 45 minutes
 '''
-# link: https://api.nasa.gov/planetary/apod?api_key=kRD0mxb0BNq9WJ7xae0BbednkdAwdg2qBR3g0iWZ
-from flask import Flask, render_template, request, session
-import os
+from flask import Flask, render_template, request
+import os, urllib, json, requests
+
+app = Flask(__name__)
+
+def readapi(file_path): # will read the txt file to get api key
+    with open(file_path, 'r') as file:
+        api_key = file.read().strip()
+    return api_key
+#api key: kRD0mxb0BNq9WJ7xae0BbednkdAwdg2qBR3g0iWZ
+api_key = readapi("key_nasa.txt")
+
+@app.route("/")
+def home_page():
+    data = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api_key}").json() # is a dictionary
+    print(data)
+    url = data["url"]
+    return render_template("main.html", url = url)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
+
